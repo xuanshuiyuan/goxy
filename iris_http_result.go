a673f0bb1a83c84919ccfe129a9610d1dc8104cd
禤水源 <xuanshuiyuan@gmail.com>
2021-11-23 11:39:27 +0800

    save code

--- iris_http_result.go
+++ iris_http_result.go
@@ -2,6 +2,15 @@ package goxy
 
 import "github.com/kataras/iris/v12/context"
 
+const (
+	StatusOK               = 0     //成功
+	StatusFail             = 40000 //失败
+	StatusParameterError   = 40001 //参数错误
+	StatusDataNotExist     = 40002 //数据不存在
+	StatusValidationFailed = 40003 //验证失败
+	StatusServerReason     = 40004 //服务器原因
+)
+
 type IrisHttpResult struct {
 	Code int64       `json:"code"`
 	Msg  string      `json:"msg"`
