ce61acbdc1c126c7628e8237662bc453974e5de3
禤水源 <xuanshuiyuan@gmail.com>
2022-01-06 11:40:21 +0800

    save code

--- common.go
+++ common.go
@@ -16,6 +16,24 @@ import (
 	"time"
 )
 
+// @Title StructAssign
+// @Description 复制结构体
+// @Author xuanshuiyuan 2022-01-06 10:25
+// @Param string 2006-01-02
+// @Return string
+func StructAssign(binding interface{}, value interface{}) {
+	bVal := reflect.ValueOf(binding).Elem() //获取reflect.Type类型
+	vVal := reflect.ValueOf(value).Elem()   //获取reflect.Type类型
+	vTypeOfT := vVal.Type()
+	for i := 0; i < vVal.NumField(); i++ {
+		// 在要修改的结构体中查询有数据结构体中相同属性的字段，有则修改其值
+		name := vTypeOfT.Field(i).Name
+		if ok := bVal.FieldByName(name).IsValid(); ok {
+			bVal.FieldByName(name).Set(reflect.ValueOf(vVal.Field(i).Interface()))
+		}
+	}
+}
+
 // @Title TimeParseInLocation
 // @Description 字符串日期格式化成时间戳
 // @Author xuanshuiyuan 2022-01-06 10:25
